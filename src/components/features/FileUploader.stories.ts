4ad47887bc613a10edadeeadaad6299ff41b9047
sergey <110@mobilon.ru>
2024-10-02 12:27:51 +0700

    fix: add stories for Profile

---
+++ src/components/features/FileUploader.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+ 
+import FileUploader from './FileUploader.vue';
+
+const meta: Meta<typeof FileUploader> = {
+  component: FileUploader,
+};
+ 
+export default meta;
+type Story = StoryObj<typeof FileUploader>;
+ 
+
+export const Standard: Story = {
+  args: {
+  },
+};
