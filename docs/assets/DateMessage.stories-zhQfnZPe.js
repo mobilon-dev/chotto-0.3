649fb71145e0a36b1bdae47962a2b32fca0506ac
sergey <110@mobilon.ru>
2024-10-04 20:38:00 +0700

    fix

--- docs/assets/DateMessage.stories-zhQfnZPe.js
+++
@@ -1,7 +0,0 @@
-import{D as r}from"./DateMessage-BzAEJgYY.js";import"./vue.esm-bundler-C2J-SQPN.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const c={component:r},e={args:{message:{text:"test"}}};var s,t,a;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    message: {
-      text: 'test'
-    }
-  }
-}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const p=["Today"];export{e as Today,p as __namedExportsOrder,c as default};
