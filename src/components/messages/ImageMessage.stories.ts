70387000e1d1cc5041ed14bb5a67702d7477c598
sergey <110@mobilon.ru>
2024-10-28 12:07:31 +0700

    fix(FileMessage): поправил status read

--- src/components/messages/ImageMessage.stories.ts
+++ src/components/messages/ImageMessage.stories.ts
@@ -9,26 +9,104 @@ const meta: Meta<typeof ImageMessage> = {
 export default meta;
 type Story = StoryObj<typeof ImageMessage>;
 
+const imageMessage = {
+  text: "Вот картинка55656",
+  url: "https://nationaltoday.com/wp-content/uploads/2022/05/Sun-Day--1200x834.jpg",
+  time: '20:55',
+};
+
+const actions = [
+  { action: 'edit', title: 'изменить', },
+  { action: 'delete', title: 'удалить', },
+];
+
+
 export const LeftImageMessage: Story = {
   args: {   
     message: { 
+      ...imageMessage,
       position: 'left',
-      text: "Вот картинка55656",
-      url: "https://nationaltoday.com/wp-content/uploads/2022/05/Sun-Day--1200x834.jpg",
-      status: 'sent',
-      time: '20:55',
     },
   },
 };
 
+export const LeftImageMessageWithSubtext: Story = {
+  args: {   
+    message: { 
+      ...imageMessage,
+      position: 'left',
+      subText: 'sub text sub text',
+    },
+  },
+};
+
+export const LeftImageMessageWithActions: Story = {
+  args: {   
+    message: { 
+      ...imageMessage,
+      position: 'left',
+      actions,
+    },
+  },
+};
+
+
 export const RightImageMessage: Story = {
   args: {   
     message: {
+      ...imageMessage,
       position: 'right',
-      text: "Вот картинка55656",
-      url: "https://nationaltoday.com/wp-content/uploads/2022/05/Sun-Day--1200x834.jpg",
+    },
+  },
+};
+
+export const RightImageMessageWithSubtext: Story = {
+  args: {   
+    message: { 
+      ...imageMessage,
+      position: 'right',
+      subText: 'sub text sub text',
+    },
+  },
+};
+
+
+export const RightImageMessageStatusSent: Story = {
+  args: {   
+    message: { 
+      ...imageMessage,
+      position: 'right',
+      status: 'sent',
+    },
+  },
+};
+
+export const RightImageMessageStatusReceived: Story = {
+  args: {   
+    message: { 
+      ...imageMessage,
+      position: 'right',
+      status: 'received',
+    },
+  },
+};
+
+export const RightImageMessageStatusRead: Story = {
+  args: {   
+    message: { 
+      ...imageMessage,
+      position: 'right',
       status: 'read',
-      time: '20:55',
+    },
+  },
+};
+
+export const RightImageMessageWithActions: Story = {
+  args: {   
+    message: { 
+      ...imageMessage,
+      position: 'right',
+      actions,
     },
   },
 };
