2b541828f7392479b9dc6e3f3d8edb81e4aa1b65
sergey <110@mobilon.ru>
2024-10-31 17:10:19 +0700

    chore: version up

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import 'primeicons/primeicons.css'
 import './assets/style.css'
 
+import { createVfm } from 'vue-final-modal'
+import 'vue-final-modal/style.css'
+
 export * from './components/messages';
 export * from './components/features';
 
