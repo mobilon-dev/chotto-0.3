423b12601c6753e71fd050259413dc12b848322d
sergey <110@mobilon.ru>
2024-11-12 22:38:30 +0700

    fix

--- src/data/messages.ts
+++ src/data/messages.ts
@@ -10,18 +10,6 @@ export const messages = [
       { action: 'edit', title: 'изменить', icon: 'https://placehold.jp/30/336633/ffffff/64x64.png?text=pin' },
       { action: 'delete', title: 'удалить' }]
   },
-  {
-    chatId: 1, type: "message.text", text: "Привет!", direction: 'outgoing', status: 'received', timestamp: '1727027959',
-    actions: [
-      { action: 'edit', title: 'изменить', icon: 'https://placehold.jp/30/336633/ffffff/64x64.png?text=pin' },
-      { action: 'delete', title: 'удалить' }]
-  },
-  {
-    chatId: 1, type: "message.image", url: "https://nationaltoday.com/wp-content/uploads/2022/05/Sun-Day--1200x834.jpg", timestamp: '1727027959',
-    alt: "Example Image", direction: 'outgoing', status: 'received', actions: [
-      { action: 'edit', title: 'изменить', icon: 'https://placehold.jp/30/336633/ffffff/64x64.png?text=pin' },
-      { action: 'delete', title: 'удалить' }]
-  },
   { chatId: 1, type: "message.text", text: "Привет! Сообщение с просмотрами",
     direction: 'outgoing', status: 'received', timestamp: '1727027959',
     views: 1000,
@@ -34,14 +22,22 @@ export const messages = [
     status: 'received', actions: [
       { action: 'edit', title: 'изменить', icon: 'https://placehold.jp/30/336633/ffffff/64x64.png?text=pin' }, 
       { action: 'delete', title: 'удалить' }] },
-  { chatId: 1, type: "message.video", url: "https://file-examples.com/storage/fe40e015d566f1504935cfd/2017/11/file_example_MP3_700KB.mp3", 
+  { chatId: 2, type: "message.video", url: "https://file-examples.com/storage/fe40e015d566f1504935cfd/2017/11/file_example_MP3_700KB.mp3", 
     text: "Привет!", direction: 'outgoing', status: 'read', timestamp: '1727027959', 
     actions: [
       { action: 'edit', title: 'изменить', icon: 'fa-edit' },
       { action: 'delete', title: 'удалить' }]
   },
   {
-    chatId: 2, messageId: 1, type: "message.system", text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    chatId: 2, 
+    messageId: 1,
+    timestamp: '1731411505',
+    type: "message.system", text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    chatId: 1, messageId: 1,
+    timestamp: 1731411505,
+    type: "message.system", text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
   {
     chatId: 2, type: "message.file", url: "https://axiomabio.com/pdf/test.pdf", filename: "Документ.pdf", direction: 'incoming',
