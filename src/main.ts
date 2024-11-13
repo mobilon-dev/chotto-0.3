b24c1a288263bd8b9e28f0408e03bd301b60193c
sergey <110@mobilon.ru>
2024-11-13 15:11:43 +0700

    fix: разнес темы по директориям

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { createApp } from 'vue';
-import App from './App.vue'; // Теперь используем корневой компонент
 import { createPinia } from 'pinia'
 
 import 'primeicons/primeicons.css'
+
+import App from './App.vue'; 
+
+import './assets/app.css'
 import './assets/style.css'
-
-
-// кастомная тема
-import './assets/themes/diamond.css'
 
 const pinia = createPinia()
 
