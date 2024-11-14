d19f4472017396386b431a01f7e3ee101882eb51
sergey <110@mobilon.ru>
2024-11-14 11:33:13 +0700

    fix: add FileDropDownMenu

--- src/library/components/FileUploader.vue
+++ src/library/components/FileUploader.vue
@@ -1,14 +1,11 @@
 <template>
-  <div>
+  <div class="container" @mouseover="onHover" @mouseout="onHoverOut">
     <div
       v-if="canUploadFile && uploadStatus !== 'success'"
       class="chat-input__button-file"
     >
       <label>
-        <input
-          type="file"
-          @change="onFileSelected"
-        >
+        <input type="file" @change="onFileSelected" />
         <span>
           <i class="pi pi-file-arrow-up" />
         </span>
@@ -16,12 +13,12 @@
     </div>
     <div v-if="!canUploadFile && uploadStatus === 'success'">
       <p>Файл загружен и готов к отправке.</p>
-      
+
       <!-- PreviewModal 
-        v-if="isModalOpen" 
+        v-if="isModalOpen"
         :previewUrl="previewUrl"
-        :isImage="isImage" 
-        :isVideo="isVideo" 
+        :isImage="isImage"
+        :isVideo="isVideo"
         :fileName="selectedFile.name"
         :handleSendEventFunction='sendFunctionWrapper'
       /-->
@@ -33,40 +30,71 @@
       <p>Ошибка при загрузке файла.</p>
     </div>
   </div>
+  <FileDropDownMenu
+    :hovered="hovered"
+    @fileSelected="handleFileChange"
+    @mouseover="onHover"
+    @mouseout="onHoverOut"
+    :canUploadFile="canUploadFile"
+  />
 </template>
 
 <script setup>
-import { ref, watch, nextTick } from 'vue';
-// import PreviewModal from './PreviewModal.vue';
-
+import { ref, watch, nextTick } from "vue";
+import PreviewModal from "./PreviewModal.vue";
+import FileDropDownMenu from "./FileDropDownMenu.vue";
 const props = defineProps({
   canUploadFile: {
     type: Boolean,
     required: true,
     default: true,
   },
-})
+});
+
+const hovered = ref(false);
+const toggleState = ref(false);
+
+const onHover = () => {
+  hovered.value = true;
+};
+
+const onHoverOut = () => {
+  hovered.value = false;
+};
 
 const selectedFile = ref(null);
-const uploadStatus = ref('');
-const fileLink = ref('');
-const previewUrl = ref('');
+const uploadStatus = ref("");
+const fileLink = ref("");
+const previewUrl = ref("");
 const isImage = ref(false);
 const isVideo = ref(false);
 const isModalOpen = ref(false);
 
-const emit = defineEmits(['fileUploaded']);
-watch(() => props.canUploadFile, ()=>{
-  nextTick(()=>{
-    if(props.canUploadFile === true) {
-      uploadStatus.value = null;
-    }
-  })
-})
+const emit = defineEmits(["fileUploaded"]);
+watch(
+  () => props.canUploadFile,
+  () => {
+    nextTick(() => {
+      if (props.canUploadFile === true) {
+        uploadStatus.value = null;
+      }
+    });
+  }
+);
 
 const onFileSelected = (event) => {
-  console.log('onFileSelected', event.target.files[0]);
+  console.log("onFileSelected", event.target.files[0]);
   selectedFile.value = event.target.files[0];
+  if (selectedFile.value) {
+    generatePreview();
+    uploadFile();
+    isModalOpen.value = true;
+  }
+};
+
+const handleFileChange = (file) => {
+  console.log("onFileSelected", file);
+  selectedFile.value = file;
   if (selectedFile.value) {
     generatePreview();
     uploadFile();
@@ -77,12 +105,12 @@ const onFileSelected = (event) => {
 const generatePreview = () => {
   const file = selectedFile.value;
   const fileType = file.type;
-  console.log(fileType)
+  console.log(fileType);
 
-  if (fileType.startsWith('image/')) {
+  if (fileType.startsWith("image/")) {
     isImage.value = true;
     isVideo.value = false;
-  } else if (fileType.startsWith('video/')) {
+  } else if (fileType.startsWith("video/")) {
     isImage.value = false;
     isVideo.value = true;
   } else {
@@ -97,37 +125,38 @@ const generatePreview = () => {
     };
     reader.readAsDataURL(file);
   } else {
-    previewUrl.value = ''; // No preview available
+    previewUrl.value = ""; // No preview available
   }
 };
 
 const uploadFile = async () => {
-  uploadStatus.value = 'uploading';
+  uploadStatus.value = "uploading";
   const formData = new FormData();
-  formData.append('file', selectedFile.value);
+  formData.append("file", selectedFile.value);
 
   try {
-    const response = await fetch('https://filebump.services.mobilon.ru/upload', {
-      method: 'POST',
-      body: formData,
-    });
+    const response = await fetch(
+      "https://filebump.services.mobilon.ru/upload",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
     const result = await response.json();
     fileLink.value = result.url;
-    uploadStatus.value = 'success';
+    uploadStatus.value = "success";
     props.canUploadFile = false;
 
     // emit event with link
-    emit('fileUploaded', {url: fileLink.value, type: 'message.file'});
-  } catch (error) {    
-    console.error('Ошибка при загрузке файла:', error);
-    uploadStatus.value = 'error';
+    emit("fileUploaded", { url: fileLink.value, type: "message.file" });
+  } catch (error) {
+    console.error("Ошибка при загрузке файла:", error);
+    uploadStatus.value = "error";
   }
 };
-
 </script>
 
 <style scoped lang="scss">
-
 .chat-input {
   &__container {
     position: relative;
@@ -161,21 +190,21 @@ const uploadFile = async () => {
 
 .new-chat-modal {
   position: fixed;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	background-color: rgba(0, 0, 0, 0.5);
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .modal-content {
   background-color: white;
-	padding: 20px;
-	border-radius: 8px;
-	width: 300px;
+  padding: 20px;
+  border-radius: 8px;
+  width: 300px;
 }
 
 .preview-image,
@@ -183,5 +212,9 @@ const uploadFile = async () => {
   max-width: 200px;
   max-height: 200px;
   margin-top: 10px;
+}
+
+.container {
+  position: relative;
 }
 </style>
