26b39a61f61eee5357d54fba0ff98b141e3acf7a
sergey <110@mobilon.ru>
2024-11-25 12:35:04 +0700

    fix

--- src/data/chats.ts
+++ src/data/chats.ts
@@ -1,12 +1,11 @@
 export const chats = [
   {
-    chatId: 1,
-    name: "Василий",
+    chatId: 1, name: "Василий",
     avatar: 'https://img.freepik.com/free-photo/smiley-man-relaxing-outdoors_23-2148739334.jpg',
     countUnread: 1,
     lastMessage: 'test',
     'lastActivity.time': 'час назад',
-    isFixed: false,
+    isFixedBottom: false,
     status: "#767676",
     'lastMessage.status': 'read',
     actions: [
@@ -15,66 +14,89 @@ export const chats = [
       { action: 'delete', title: 'удалить', icon: 'https://placehold.jp/30/dd6699/ffffff/64x64.png?text=del' },
     ],
     typing: true,
+    metadata: {
+      timestamp: ''
+    }
   },
   {
-    chatId: 2, name: "Мария", countUnread: 0, isFixedTop: true,
-    'lastActivity.time': 'час назад',
-    'lastMessage.status': 'received',
+    chatId: 2, name: "Мария",
+    countUnread: 0, isFixedTop: true,
+    'lastActivity.time': 'час назад', 'lastMessage.status': 'received',
     actions: [
       { action: 'edit', title: 'изменить' }, { action: 'unpin', title: 'открепить' },
     ]
   },
   {
     chatId: 3, name: "Анна",
-    countUnread: 0,
+    countUnread: 0, isFixedBottom: true,
     'lastMessage.status': 'se',
-    isFixedBottom: true, actions: [
+    actions: [
       { action: 'edit', title: 'изменить' }, { action: 'unpin', title: 'открепить' },
     ]
   },
   {
-    chatId: 4, name: "Василий", countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад', statusMessage: 'received', isFixed: false, status: "#00b972", actions: [
+    chatId: 4, name: "Василий", 
+    countUnread: 0, lastMessage: 'test', 
+    'lastActivity.time': 'час назад', 'lastMessage.status': 'received',  
+    status: "#00b972", actions: [
       { action: 'edit', title: 'изменить' }, { action: 'delete', title: 'удалить' },
     ]
   },
   {
-    chatId: 5, name: "Василий", countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад', statusMessage: 'send', isFixed: false, actions: [
+    chatId: 5, name: "Василий", 
+    countUnread: 0, lastMessage: 'test', 
+    'lastActivity.time': 'час назад', 'lastMessage.status': 'send',  
+    actions: [
       { action: 'edit', title: 'изменить' }, { action: 'delete', title: 'удалить' },
     ]
   },
   {
-    chatId: 6, name: "Василий", countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад', isFixed: false, actions: [
+    chatId: 6, name: "Василий", 
+    countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад',
+    actions: [
       { action: 'edit', title: 'изменить' }, { action: 'delete', title: 'удалить' },
     ]
   },
   {
-    chatId: 7, name: "Василий 2", countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад', isFixed: false, actions: [
+    chatId: 7, name: "Василий 2", 
+    countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад',
+    actions: [
       { action: 'edit', title: 'изменить' }, { action: 'delete', title: 'удалить' },
     ],
     typing: true,
   },
   {
-    chatId: 8, name: "Василий", countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад', isFixed: false, actions: [
+    chatId: 8, name: "Василий",
+    countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад',
+    actions: [
       { action: 'edit', title: 'изменить' }, { action: 'delete', title: 'удалить' },
     ]
   },
   {
-    chatId: 9, name: "Василий", countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад', isFixed: false, actions: [
+    chatId: 9, name: "Василий", 
+    countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад',
+    actions: [
       { action: 'edit', title: 'изменить' }, { action: 'delete', title: 'удалить' },
     ]
   },
   {
-    chatId: 10, name: "Василий", countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад', isFixed: false, actions: [
+    chatId: 10, name: "Василий",
+    countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад',
+    actions: [
       { action: 'edit', title: 'изменить' }, { action: 'delete', title: 'удалить' },
     ]
   },
   {
-    chatId: 11, name: "Василий", countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад', isFixed: false, actions: [
+    chatId: 11, name: "Василий",
+    countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад',
+    actions: [
       { action: 'edit', title: 'изменить' }, { action: 'delete', title: 'удалить' },
     ]
   },
   {
-    chatId: 12, name: "Василий", countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад', isFixed: false, actions: [
+    chatId: 12, name: "Василий", 
+    countUnread: 0, lastMessage: 'test', 'lastActivity.time': 'час назад',
+    actions: [
       { action: 'edit', title: 'изменить' }, { action: 'delete', title: 'удалить' },
     ]
   },
