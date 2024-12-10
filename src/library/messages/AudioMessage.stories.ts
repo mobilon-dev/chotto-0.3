7fb8f1b1d196d07a8aa2790d6efa0a604716db48
sergey <110@mobilon.ru>
2024-12-10 10:40:21 +0700

    fix: stories with transcript

--- src/library/messages/AudioMessage.stories.ts
+++ src/library/messages/AudioMessage.stories.ts
@@ -143,3 +143,34 @@ export const LeftMessageWithActionsAndTextWithLink: Story = {
     },
   },
 };
+
+
+export const RightMessageWithTranscript: Story = {
+  args: {
+    message: {
+      ...message,
+      position: 'right',
+      actions,
+      transcript: {
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor '+
+          'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, '+
+          'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. yandex.ru',
+      },
+    },
+  },
+};
+
+export const LeftMessageWithTranscript: Story = {
+  args: {
+    message: {
+      ...message,
+      position: 'left',
+      actions,
+      transcript: {
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor '+
+          'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, '+
+          'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. yandex.ru',
+      },
+    },
+  },
+};
