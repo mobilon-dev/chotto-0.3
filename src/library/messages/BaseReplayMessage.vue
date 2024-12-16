2680e2e2796685588beccbc7cca1d4254157d50d
determaer <determaer2012@gmail.com>
2024-12-16 14:50:34 +0700

    fix: base reply clean

--- src/library/messages/BaseReplayMessage.vue
+++ src/library/messages/BaseReplayMessage.vue
@@ -24,43 +24,11 @@ import {
 } from '../../types';
 
 defineProps({
-  /*
-  audioMessage: {
-    type: Object as () => IAudioMessage,
-    required: false,
-  },
-  videoMessage: {
-    type: Object as () => IVideoMessage,
-    required: false,
-  },
-  fileMessage: {
-    type: Object as () => IFileMessage,
-    required: false,
-  },
-  imageMessage: {
-    type: Object as () => IImageMessage,
-    required: false,
-  },
-  textMessage: {
-    type: Object as () => ITextMessage,
-    required: false,
-  },
-  */
   message: {
     type: Object as () => IFeedObject,
   }
 });
-/*
-const componentName = computed(() => {
-  console.log(props.message)
-  if (props.textMessage) return ReplayTextMessage;
-  if (props.imageMessage) return ReplayImageMessage;
-  if (props.fileMessage) return ReplayFileMessage;
-  if (props.audioMessage) return ReplayAudioMessage;
-  if (props.videoMessage) return ReplayVideoMessage;
-  return null;
-});
-*/
+
 const componentsMap = (type) => {
 const r = {
   'message.text': ReplayTextMessage,
@@ -71,16 +39,7 @@ const r = {
 };
 return r[type];
 }
-/*
-const selectedMessage = computed(() => {
-  if (props.textMessage) return props.textMessage;
-  if (props.imageMessage) return props.imageMessage;
-  if (props.fileMessage) return props.fileMessage;
-  if (props.audioMessage) return props.audioMessage;
-  if (props.videoMessage) return props.videoMessage;
-  return null;
-});
-*/
+
 </script>
 
 <style lang="scss"></style>
