f429c24e551c32cea388a3931cff9c816c0e800f
john_doe <rusacuma306@gmail.com>
2025-09-02 21:31:21 +0700

    add some data examples with comments

--- src/data/channels.ts
+++ src/data/channels.ts
@@ -8,4 +8,41 @@ export const channels = [
     channelId: 'channel2', 
     title: 'test channel 2' 
   },
+
+  // Примеры структуры каналов из приложения
+  {
+    branchId: "bch_1",
+    channelId: "whatsapp.W1243",  // тип канала может определяться по префиксу channelId
+    title: "whatsapp 73910001100 (default)",
+    status: "active",
+    autoAccess: false
+  },
+  {
+    branchId: "bch_1",
+    channelId: "whatsapp.W5432",
+    title: "whatsapp 79130001111",
+    status: "active",
+    autoAccess: false
+  },
+  {
+    branchId: "bch_1",
+    channelId: "waba.WABA7534",
+    title: "Отдел продаж 79560002200",
+    status: "active",
+    autoAccess: false
+  },
+  {
+    branchId: "bch_1",
+    channelId: "telegram.T6432",
+    title: "telegram 79830003300",
+    status: "active",
+    autoAccess: false
+  },
+  {
+    branchId: "bch_1",
+    channelId: "telegrambot.TGB5431",
+    title: "TG Bot №14",
+    status: "active",
+    autoAccess: false
+  }
 ];
