9970b7266b3e8f4447fb0688e6de185149aa0759
Elena Pischurova <boombosha@gmail.com>
2025-09-15 15:41:41 +0300

    refactor(chat-input): move send icons to local folder and update imports

---
+++ src/components/compounds/ChatInput/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as WhatsAppSendIcon } from './WhatsAppSendIcon.vue';
+export { default as TelegramSendIcon } from './TelegramSendIcon.vue';
+export { default as MaxSendIcon } from './MaxSendIcon.vue';
+export { default as SMSSendIcon } from './SMSSendIcon.vue';
