58ae40a4f83c035f0cb9fbc7452dd14b9ef2bdff
Elena Pischurova <boombosha@gmail.com>
2025-09-15 17:33:46 +0300

    refactor(atoms): extract component styles to separate SCSS files

--- src/components/atoms/ModalFullscreen/ModalFullscreen.vue
+++ src/components/atoms/ModalFullscreen/ModalFullscreen.vue
@@ -56,46 +56,5 @@ onUnmounted(() => {
 </script>
 
 <style scoped lang="scss">
-.modal {
-  &__container{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 101;
-    color: var(--chotto-primary-text-color);
-    background-color: var(--chotto-modal-bg);
-    border-radius: var(--chotto-modal-border-radius);
-    padding: var(--chotto-modal-padding);
-    max-width: 95%;
-    width: fit-content;
-    box-shadow: var(--chotto-modal-overlay-shadow);
-  }
-    
-  &__overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: var(--chotto-modal-mask-background);
-    z-index: 1000;
-  }
-
-  &__close-button {
-    display: block;
-    background-color: transparent;
-    border: none;
-    padding: 4px;
-    margin: 0 0 14px auto;
-    cursor: pointer;
-
-    span {
-      color: var(--chotto-secondary-text-color);
-      font-size: var(--chotto-button-icon-size);
-    }
-  }
-}
-
-  
+@use './styles/ModalFullscreen.scss';
 </style>
--- src/components/atoms/ModalNoFooter/ModalNoFooter.vue
+++ src/components/atoms/ModalNoFooter/ModalNoFooter.vue
@@ -38,43 +38,6 @@ const closeModalOutside = (evt) => {
   </transition>
 </template>
 
-<style scoped>
-.modal-backdrop {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.3);
-    display: flex;
-    justify-content: center;  
-    align-items: center;
-    z-index:1000;
-}
-
-.modal {
-    background: var(--chotto-modal-bg);
-    color: var(--chotto-primary-text-color);
-    font-family: var(--chotto-container-font-family);
-    overflow-x: auto;
-    display: flex; 
-    flex-direction: column;
-    position: relative;
-    padding: 20px 10px;
-}
-
-.buttons {
-  display: block;
-  margin: 10px 10px 10px 10px;
-}
-
-.modal-fade-enter,
-.modal-fade-leave-to {
-    opacity: 0;
-}
-
-.modal-fade-enter-active,
-.modal-fade-leave-active {
-    transition: opacity 0.5s ease;
-}
+<style scoped lang="scss">
+@use './styles/ModalNoFooter.scss';
 </style>
