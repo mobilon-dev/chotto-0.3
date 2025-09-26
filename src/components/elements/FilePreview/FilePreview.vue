edfed4d03dcaeb095e33cdc0d33715e67e1b5e79
Elena Pischurova <boombosha@gmail.com>
2025-09-26 15:38:13 +0300

    fix(FilePreview): add explicit type to formatTime function

--- src/components/elements/FilePreview/FilePreview.vue
+++ src/components/elements/FilePreview/FilePreview.vue
@@ -155,7 +155,7 @@ function togglePlayPause() {
   }
 }
 
-const formatTime = (time) => {
+const formatTime = (time: number) => {
   const minutes = Math.floor(time / 60);
   const seconds = Math.floor(time % 60);
   return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
