c05ba03fb6ed1652e9a675820fc9276f2b42f3a3
Elena Pischurova <boombosha@gmail.com>
2025-09-26 15:53:52 +0300

    fix(sortByTimestamp): add explicit type annotation to function

--- src/helpers/sortByTimestamp.ts
+++ src/helpers/sortByTimestamp.ts
@@ -1,4 +1,4 @@
-export const sortByTimestamp = (messages) => {
+export const sortByTimestamp = (messages: Array<{ timestamp: string | number }>) => {
   return messages.sort((a, b) => {
     if (Number(a.timestamp) < Number(b.timestamp)) return -1;
     if (Number(a.timestamp) > Number(b.timestamp)) return 1;
